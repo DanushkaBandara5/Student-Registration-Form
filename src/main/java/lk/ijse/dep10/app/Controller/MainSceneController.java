db0c2ba6bcf0f660e240179fa037ace94f621666
danushkakasun5 <danushkakasun5@gmail.com>
2023-07-15 16:13:04 +0530

    finalize the UI

--- src/main/java/lk/ijse/dep10/app/Controller/MainSceneController.java
+++ src/main/java/lk/ijse/dep10/app/Controller/MainSceneController.java
@@ -1,4 +1,73 @@
-package lk.ijse.dep10.app.Controller;
+package lk.ijse.dep10.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseDragEvent;
 
 public class MainSceneController {
+
+    @FXML
+    private Button btnBrowse;
+
+    @FXML
+    private Button btnClear;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnNew;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private ImageView imgPro;
+
+    @FXML
+    private TableView<?> tblStudent;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtSearch;
+
+    @FXML
+    void btnBrowseOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClearOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNewOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSavaOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tblStudentOnKEyRelaesed(MouseDragEvent event) {
+
+    }
+
 }
